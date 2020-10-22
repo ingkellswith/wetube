629337fb1fd50c383dab3aceffe50164f2f9aac8
ingkellswith <allover3773@gmail.com>
2020-10-22 22:27:05 +0900

    update

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-#WeTube
+# WeTube
 
-Cloning Youtube with VanillaJS and NodeJS
 
-#pages
+Cloning Youtube with Vanilla and NodeJS
 
--[]Home
+## Pages:
+
+- [ ] Home
+- [ ] Join
+- [ ] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,9 +9,9 @@ import routes from "../routes";
 
 const userRouter = express.Router();
 
-//userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
+//editProfile보다 userDetail먼저 오면 id로 인식하기 때문에 순서가 중요
 
 export default userRouter;
